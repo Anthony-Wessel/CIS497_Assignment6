c2704d1a7b4c1da016802626f60e97b16059bc3c
Anthony <awessel@mail.bradley.edu>
2020-10-20 16:13:11 -0500

    Finished follow along video

---
+++ OOPinUnity/Assets/Scripts/Golem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golem : Enemy
+{
+
+    protected int damage;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        health = 120;
+
+        GameManager.Instance.score = 5;
+    }
+
+    protected override void Attack(int amount)
+    {
+        Debug.Log("Golem attacks");
+    }
+
+    public override void TakeDamage(int amount)
+    {
+        Debug.Log("You took " + amount + " damage");
+    }
+}
---
+++ OOPinUnity/Assets/Scripts/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public int damageBonus;
+
+    public Enemy enemyHoldingWeapon;
+
+    public void Recharge()
+    {
+        Debug.Log("Recharging Weapon");
+    }
+
+    private void Awake()
+    {
+        enemyHoldingWeapon = gameObject.GetComponent<Enemy>();
+        EnemyEatsWeapon(enemyHoldingWeapon);
+    }
+
+    protected void EnemyEatsWeapon(Enemy enemy)
+    {
+        Debug.Log("Enemy eats weapon");
+    }
+}
